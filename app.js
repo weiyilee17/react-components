ee226297cac250897f49df117b264b0c17c767db
Hack Reactor Student <student@hackreactor.com>
2017-07-19 14:38:14 -0700

    Creating and rendering nested React components

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // TODO
+
+var App = () => (
+  <div>
+    <h1>Grocery list
+    </h1>
+    <ul>
+      < Cucumber />
+      < HotDogs />
+    </ul>
+  </div>
+);
+
+var Cucumber = () => (
+  <li>Cucumber</li>
+);
+
+var HotDogs = () => (
+  <li>HotDogs</li>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
