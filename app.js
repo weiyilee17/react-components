2882e37cf330dbb6d9d1512079f0edff7e5fb3b4
Hack Reactor Student <student@hackreactor.com>
2017-07-19 15:32:30 -0700

    Complete React Components sprint

--- app.js
+++ app.js
@@ -4,21 +4,8 @@ var App = () => (
   <div>
     <h1>Grocery list
     </h1>
-    <GroceryList groceryItems = {['bacon', 'HotDogs', 'Cucumber']}/>
+    <GroceryList groceryItems = {['bacon', 'HotDogs', 'Cucumber', 'chicken']}/>
   </div>
-);
-
-var Cucumber = () => (
-  <li>Cucumber</li>
-);
-
-var HotDogs = () => (
-  <li>HotDogs</li>
-);
-
-var GroceryListItem = (props) => (
-
-  <li>{props.name}</li>
 );
 
 var GroceryList = (props) => (
@@ -28,5 +15,41 @@ var GroceryList = (props) => (
   })}
   </ul>
 );
+
+// A class component can be defined as an ES6 class
+// that extends the base Component class included in the React library
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // `state` is just an object literal
+    this.state = {
+      done: false
+    };
+  }
+
+  // When a list item is hovered, we will toggle the `done`
+  // boolean, and our component's `render` method will run again
+  onListItemMouseOver() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    // Making the style conditional on our `state` lets us
+    // update it based on user interactions.
+    var style = {
+      'font-weight': this.state.done ? 'bold' : 'normal'
+    };
+
+    // You can pass inline styles using React's `style` attribute to any component
+    // snake-cased css properties become camelCased this this object
+    return (
+      <li style={style} onMouseOver={this.onListItemMouseOver.bind(this)}>{this.props.name}</li>
+    );
+  }
+}
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
